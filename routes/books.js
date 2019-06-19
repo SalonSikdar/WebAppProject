a846b544906403cc2069e422963072f8d1543269
Saloni Sikdar <saloni.sikdar@mountblue.io>
2019-06-19 10:41:26 +0530

    i

--- routes/books.js
+++ routes/books.js
@@ -4,4 +4,7 @@ const router = express.Router();
 // Books Page 
 router.get('/books',(req,res) => res.send('Books'));
 
+// Book Page 
+router.get('/book',(req,res) => res.send('Book'));
+
 module.exports = router;
