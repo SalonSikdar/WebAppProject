6c7d1d69850dc230646b42e7d891d6751e7d3a47
yatinburhmi <yatinburhmi@gmail.com>
2019-06-27 08:39:16 +0530

    individual routing done

--- routes/authors.js
+++ routes/authors.js
@@ -11,15 +11,30 @@ router.get("/authors", (req, res) => {
       password: "Mithi@23",
       database: "goodReads"
     });
-    console.log("DB Connected");
+    console.log("DB Connected in authors");
     const [rows, fields] = await connection.execute("SELECT * FROM `authors`");
     res.render("authors", { authors: rows });
   })();
 });
 
 // Author Page
-router.get("/authors/:userQuery", (req, res) =>
-  res.render("author", { data: { userQuery: req.params.userQuery } })
-);
+router.get("/authors/:userQuery", (req, res) =>{
+  (async function main() {
+    const connection = await mysql.createConnection({
+      host: "localhost",
+      user: "saloni",
+      password: "Mithi@23",
+      database: "goodReads"
+    });
+    console.log("DB Connected in authors");
+    const [rows, fields] = await connection.execute("SELECT * FROM `authors`");
+    res.render("author", { authors : rows , data: { userQuery: req.params.userQuery } });
+  })();
+});
+
+
+// router.get("/authors/:userQuery", (req, res) =>
+//   res.render("author", { data: { userQuery: req.params.userQuery } })
+// );
 
 module.exports = router;
--- routes/books.js
+++ routes/books.js
@@ -1,12 +1,25 @@
-const express = require('express');
-
+const express = require("express");
+const mysql = require("mysql2/promise");
 const router = express.Router();
 
-
 // Books Page
-router.get('/books', (req, res) => res.render('books'));
+router.get("/books", (req, res) => {
+  (async function main() {
+    const connection = await mysql.createConnection({
+      host: "localhost",
+      user: "saloni",
+      password: "Mithi@23",
+      database: "goodReads"
+    });
+    console.log("DB Connected in books");
+    const [rows, fields] = await connection.execute("SELECT * FROM `books`");
+    res.render("books", { books: rows });
+  })();
+});
 
 // Book Page
-router.get('/books/:userQuery', (req, res) => res.render('book',{data : {userQuery: req.params.userQuery }}));
+router.get("/books/:userQuery", (req, res) =>
+  res.render("book", { data: { userQuery: req.params.userQuery } })
+);
 
 module.exports = router;
